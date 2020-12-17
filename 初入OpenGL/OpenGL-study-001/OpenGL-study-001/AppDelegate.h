6e58aea701794dfed31e18aaf52b74fff0f89895
ClintonYanglolo <dulpyhb@gmail.com>
2020-12-17 23:19:12 +0800

    no message

---
+++ 初入OpenGL/OpenGL-study-001/OpenGL-study-001/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  OpenGL-study-001
+//
+//  Created by 杨皓博 on 2020/12/17.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ 初入OpenGL/OpenGL-study-001/OpenGL-study-001/SceneDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SceneDelegate.h
+//  OpenGL-study-001
+//
+//  Created by 杨皓博 on 2020/12/17.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
